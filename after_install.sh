c31b2db2ea7bce776c3f200d3155e86ed03460d9
Aktoch <aktoch0@gmail.com>
2020-08-15 07:15:42 -0300

    Adicionado Vagrantfiles, Atualizando o script de instalaçao e o README

--- after_install.sh
+++ after_install.sh
@@ -1,20 +1,49 @@
 #!/usr/bin/env bash
-# ----------------------------- VARIÁVEIS ----------------------------- #
-#PPAs
+#
+# after_INSTALAR.sh - Pos-instalação do Ubuntu 20.04
+#
+# github      https://www.github.com/Aktoch
+# Autor:      João "Aktoch" Florencio
+# Manutenção: João "Aktoch" Florencio
+#
+#    Observações usuario que executar precisa de tem permisão de sudo
+#    Este programa irá instalar os pacotes que uso no meu dia a dia
+#    Exemplos:
+#    $ ./after_INSTALAR.sh -i
+#    Neste exemplo o script será executado no modo de instalação que instala tudo.
+
+# Testado em:
+#   bash  5.0.17
+# ------------------------------- VARIÁVEIS ----------------------------------------- #
+MENSAGEM_USO="
+    $(basename $0) - Para pos instalação do ubuntu 20.04
+    $(basename $0) - [OPÇÕES]
+
+    -h - Menu de ajda
+    -i - instala tanto os pacote do APT quanto os .deb
+    -d - instala somente .deb
+    -l - instala somente o lutris
+    -p - Adiciona as PPA'S
+    -t - Update e Upgrade.
+"
+MENSAGEM_ERRO="
+     Nenhuma opção informada valida verifique o -h
+"
+#PPA's
 PPA_LUTRIS="ppa:lutris-team/lutris"
 PPA_PYTHON="ppa:deadsnakes/ppa"
-#URLS testa depois para impremetar um vetor.
+#Chaves flags.
+KEY_LUTRIS=0
+KEY_DOWNLOAD=0
+KEY_INSTALAR=0
+KEY_UPDATE=0
+#Pacotes .deb que serao baixados para instalar.
 URL_DOWNLOAD=(
     https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
     https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.2.5.40859-focal_amd64.deb
 )
-URL_GIT_ICONS="https://github.com/daniruiz/flat-remix"
-URL_GIT_THEMES="https://github.com/daniruiz/flat-remix-gtk"
-#Pasta que serao que usadas.
-DIR_DOWNLOAD="$HOME/Downloads/apps"
-THEMES="$HOME/projects"
-#Programas que serão instalados / alterar quem quiser
+#Pacotes instalados pelo APT
 PROGRAMAS_PARA_INSTALAR=(
     chrome-gnome-shell
     flameshot
@@ -25,7 +54,7 @@ PROGRAMAS_PARA_INSTALAR=(
     gnome-tweaks
     net-tools
     nmap
-    nvim
+    neovim
     piper
     powerline
     python3
@@ -41,67 +70,51 @@ PROGRAMAS_PARA_INSTALAR=(
     vlc
     zsh
 )
-# ----------------------------- REQUISITOS ----------------------------- #
-#Adicionando a x32
+# ------------------------------- FUNÇÕES ----------------------------------------- #
+Ppa() {
+    sudo apt-add-repository "$PPA_PYTHON" -y
+}
+Lutris() {
+    sudo add-apt-repository "$PPA_LUTRIS" -y && sudo apt INSTALAR-y lutris
+}
+Download() {
+    mkdir -p "$DIR_DOWNLOAD"
+    for idown in ${URL[@]}; do
+        wget -c "$idown" -P "$DIR_DOWNLOAD"
+    done
+    sudo dpkg -i $DIR_DOWNLOAD/*.deb
+}
+Snap() {
+    sudo snap INSTALAR code --classic
+    sudo snap INSTALAR discord
+}
+Instalacao() {
+    Ppa
+    for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
+        if ! dpkg -l | grep -q $nome_do_programa; then
+            sudo apt INSTALAR "$nome_do_programa" -y
+        else
+            echo "[INSTALADO] - $nome_do_programa"
+        fi
+    done
+    Download
+    Snap
+}
+# ------------------------------- EXECUÇÃO ----------------------------------------- #
+while test -m "$1"; do
+    case "S1" in
+    -h) echo "$MENSAGEM_USO" && exit 0 ;;
+    -i) KEY_INSTALAR=1 ;;
+    -d) KEY_DOWNLOAD=1 ;;
+    -l) KEY_LUTRIS=1 ;;
+    -t) KEY_UPDATE=1 ;;
+    *) echo "$MENSAGEM_ERRO" && exit 1 ;;
+    esac
+    shift
+done
 sudo dpkg --add-architecture i386
 sudo apt update -y
-#adicionando o ppa do python
-sudo apt-add-repository "$PPA_PYTHON" -y
-
-# ----------------------------- EXECUÇÃO ----------------------------- #
-sudo apt update -y
-#verificando se tem a variavem or not e instalando o lutris.
-[[ "$*" = "-lutris"]] && sudo add-apt-repository "$PPA_LUTRIS" -y && sudo apt install-y lutris
-
-mkdir -p "$DIR_DOWNLOAD"
-for idown in ${URL[@]}; do
-wget -c "$idown" -P "$DIR_DOWNLOAD"
-done
-
-#instalando os .deb baixado
-sudo dpkg -i $DIR_DOWNLOAD/*.deb
-sudo apt install -f -y
-
-# Instalar programas no apt
-for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
-    if ! dpkg -l | grep -q $nome_do_programa; then
-        sudo apt install "$nome_do_programa" -y
-    else
-        echo "[INSTALADO] - $nome_do_programa"
-    fi
-done
-
-## Instalando pacotes Flatpak and Snap ##
-sudo snap install code --classic
-sudo snap install discord
-#sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-#flatpak install -y flathub com.discordapp.Discord
-#flatpak install -y flathub com.visualstudio.code
-
-#----------------themes-----------------#
-#Em fazer de teste
-mkdir "$THEMES"
-mkdir -p ~/.icons && mkdir -p ~/.themes
-cd "$DIR_DOWNLOAD"
-git clone "$URL_GIT_ICONS"
-git clone "$URL_GIT_THEMES"
-cp -r flat-remix/Flat-Remix* ~/.icons/ && cp -r flat-remix-gtk/Flat-Remix-GTK* ~/.themes
-
-#git clone "$DOTFILES"
-
-#OH my zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-#Vagrant box add
-vagrant box add kalilinux/rolling
-vagrant box add hashicorp/bionic64
-vagrant box add debian/buster64
-vagrant box add centos/7
-
-# ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
-## Finalização, atualização e limpeza##
-sudo snap remove snap-store
-sudo apt update && sudo apt dist-upgrade -y
-sudo apt autoclean
-sudo apt autoremove -y
-rm -r "$DIR_DOWNLOAD"
+[ $KEY_INSTALAR -eq 1 ] && Instalacao && exit 0
+[ $KEY_DOWNLOAD -eq 1 ] && Download && exit 0
+[ $KEY_LUTRIS -eq 1 ] && Lutris && exit 0
+[ $KEY_UPDATE -eq 1 ] && sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y

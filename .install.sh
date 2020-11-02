01982d7772cbebb4ef414d41d6656e926e9d7e92
aktoch <aktoch@protonmail.com>
2020-11-02 20:36:40 -0300

    Update no instalador

--- .install.sh
+++ .install.sh
@@ -1,16 +1,6 @@
-echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> .bashrc
 git clone --bare https://github.com/Aktoch/dotfiles $HOME/.cfg
-source .bashrc
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
 config checkout
 config config status.showUntrackedFiles no

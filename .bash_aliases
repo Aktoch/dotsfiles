99ba68602c144b7cd07441de9787b43f99a4e930
joao florencio <joao.florencio@pm.me>
2021-01-14 23:24:42 -0300

    updatee

--- .bash_aliases
+++ .bash_aliases
@@ -1,2 +1,3 @@
 alias l='ls -lha'
 alias update='sudo apt update && sudo apt upgrade -y'
+alias music='~/.local/bin/youtube-dl --extract-audio --audio-format mp3'
--- install.sh
+++ install.sh
@@ -12,32 +12,6 @@ URL=(
     'https://extensions.gnome.org/extension/2120/sound-percentage/'
     'https://extensions.gnome.org/extension/906/sound-output-device-chooser/'
 )
-#Packages installed by APT
-PACKAGES=(
-    chrome-gnome-shell
-    flameshot
-    fonts-hack-ttf
-    fonts-powerline
-    git
-    gnome-tweaks
-    nmap
-    vim
-    neovim
-    powerline
-    python3
-    python3-pip
-    python-is-python3
-    snapd    
-    tilix
-    transmission
-    tmux
-    vagrant
-    virtualbox
-    vlc
-    zsh
-    fdupes
-    neofetch
-)
 BOX=(
     debian/buster64
     kalilinux/rolling
@@ -55,28 +29,17 @@ EOF
 Install() {
     sudo dpkg --add-architecture i386
     sudo apt remove snapd --purge -y
-    for name_program in ${PACKAGES[@]}; do
-        if ! dpkg -l | grep -q $name_program; then
-            sudo apt install "$name_program" -y
-        else
-            echo "[INSTALLED] - $name_program"
-        fi
-    done
+    sudo apt update
+    sudo apt install -y ffmpeg chrome-gnome-shell flameshot fonts-hack-ttf fonts-powerline git gnome-tweaks nmap vim neovim powerline python3 python3-pip python-is-python3 snapd tilix transmission tmux vagrant virtualbox vlc zsh fdupes neofetch steam apt-transport-https curl gnupg brave-browser code
 }
 Vscode() {
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
     sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-    sudo apt install apt-transport-https
-    sudo apt update
-    sudo apt install code
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'    
 }
 Brave() {
-    sudo apt install apt-transport-https curl gnupg
     curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
     echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    sudo apt update
-    sudo apt install brave-browser
 }
 Ohmyzsh() {
     sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -92,21 +55,15 @@ Vagrant() {
 Openlinks() {
     firefox "${URL[@]}"
 }
-Steam() {
-    sudo apt install -y flatpak gnome-software-plugin-flatpak
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    flatpak install flathub com.valvesoftware.Steam
-}
 # ------------------------------- EXECUTION ----------------------------------------- #
 sudo apt update
 Sshkey
 Ppa
-Install
-Steam
 Vscode
 Brave
+Install
 Ohmyzsh
 Snap
 Vagrant
-OpenLinks
+Openlinks
 sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y

b57ea9e9dd22e0a5c7fedd1df88f17b74a56091f
João Florencio <aktoch@pm.me>
2021-05-17 23:08:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,38 +2,9 @@
 # Autor:      João "Aktoch" Florencio
 # github      https://www.github.com/Aktoch
 
-APPS=(
-    ffmpeg
-    chrome-gnome-shell
-    flameshot
-    fonts-hack-ttf
-    fonts-powerline
-    git
-    gnome-tweaks
-    nmap
-    vim
-    neovim
-    powerline
-    python3
-    python3-pip
-    tilix
-    transmission
-    tmux
-    virtualbox
-    vagrant    
-    vlc
-    zsh
-    fdupes
-    neofetch
-    steam
-    brave-browser
-    code
-)
-
-
 ssh-keygen -t ed25519 -a 100
 
-sudo apt install apt-transport-https curl gnupg -y
+sudo apt install -y apt-transport-https curl gnupg git ffmpeg chrome-gnome-shell flameshot fonts-hack-ttf fonts-powerline gnome-tweaks nmap vim tilix tmux virtualbox vagrant vlc zsh fdupes neofetch steam 
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
@@ -43,7 +14,4 @@ sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 
-sudo apt update    
-for counter in ${APPS[@]}; do
-  sudo apt install -y "$counter"
-done
+sudo apt install -y brave-browser code
